1c558262f76e68fbab9edb62679babf37fc213f5
Paul Vincent Sablan <vincentpaul_012@yahoo.com>
2017-12-04 02:31:24 +0800

    Modal edited but problem with external css

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,38 +1,17 @@
-// SLIDER
-function updateTextInput(val) {
-          document.getElementById('resultVal').value=val; 
-          if(val==1){
-            $("<style type='text/css'>#result::-webkit-slider-thumb{background-color:rgba(255,0,0, 0.75)}</style>").appendTo($("head"));
-          }
-          if(val==-1){
-            $("<style type='text/css'>#result::-webkit-slider-thumb{background-color:rgba(0,0,255, 0.75)}</style>").appendTo($("head"));
-          }
-          if(val==0){
-            $("<style type='text/css'>#result::-webkit-slider-thumb{background-color:rgba(238,130,238, 0.75)}</style>").appendTo($("head"));
-          }
-}
-//read excel data
-function readFile(){ 
-                    var filename = document.getElementById('openfile').files[0];
-                    document.getElementById('#filepath').value = filename;
-                     var fileReader = new FileReader();
-                    fileReader.onload = function(fileLoadedEvent)
-                    {
-                        var textFromFileLoaded = fileLoadedEvent.target.result;
-                        document.getElementById("file_content").value = textFromFileLoaded;
-                    };
+$(function()
+    {
+        $('#openfile').on('click',function ()
+        {
+            var filePath = $(this).val();
+            $('#test').val() = filePath;
+        });
 
-                    try{
-                         fileReader.readAsText(fileToLoad, "UTF-8");
-                    }
-                    catch(err){
-                        alert("Please choose some file...")
-                    }
 
-}
-//modal
+        // Get the modal
+        $(".btn-detect").click(function(){
+          $(".modal").modal('show');
+        });
 
-// Get the modal
-$(".btn-detect").click(function(){
-  $(".modal").modal('show');
+
 });
+
