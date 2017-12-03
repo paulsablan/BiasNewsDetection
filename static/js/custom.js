dd26cc754e1994e13258d6d8d7ae97f217943e22
Paul Vincent Sablan <vincentpaul_012@yahoo.com>
2017-12-03 20:10:13 +0800

    Modal edited

--- static/js/custom.js
+++ static/js/custom.js
@@ -13,34 +13,26 @@ function updateTextInput(val) {
 }
 //read excel data
 function readFile(){ 
-                    var filename = document.getElementById('#openfile').value;
-                   
+                    var filename = document.getElementById('openfile').files[0];
+                    document.getElementById('#filepath').value = filename;
+                     var fileReader = new FileReader();
+                    fileReader.onload = function(fileLoadedEvent)
+                    {
+                        var textFromFileLoaded = fileLoadedEvent.target.result;
+                        document.getElementById("file_content").value = textFromFileLoaded;
+                    };
+
+                    try{
+                         fileReader.readAsText(fileToLoad, "UTF-8");
+                    }
+                    catch(err){
+                        alert("Please choose some file...")
+                    }
 
 }
 //modal
 
 // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+$(".btn-detect").on("click","load","change",function(){
+  $(".modal").modal('toggle');
+});
