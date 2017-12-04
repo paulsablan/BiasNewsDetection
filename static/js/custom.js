fd174d8f9851ecef7cf50b728bfa0269dbaf59e4
Paul Vincent Sablan <vincentpaul_012@yahoo.com>
2017-12-04 14:03:06 +0800

    Modal edited

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,6 +7,11 @@ $(function()
         });
 
 
+        $('#openfile').change(function(e){
+            var fileName = e.target.files[0].name;
+            alert('The file "' + fileName +  '" has been selected.');
+            document.getElementById('#testnew').value = filename
+        });
         // Get the modal
         $(".btn-detect").click(function(){
           $(".modal").modal('show');
